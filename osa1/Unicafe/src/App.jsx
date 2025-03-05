640d834df18440392ed5174cd4f2d41999645fea
vemaveij <venla.m.veijalainen@jyu.fi>
2025-03-05 15:31:01 +0200

    1.7

--- osa1/Unicafe/src/App.jsx
+++ osa1/Unicafe/src/App.jsx
@@ -20,16 +20,42 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
   const handleGoodClick = () => {
+    
     setGood(good + 1)
+    setAll(all + 1)
+    countAverage(good+1, neutral, bad)
+    countPositive(good+1, neutral, bad)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
+    countAverage(good, neutral +1, bad)
+    countPositive(good, neutral+1, bad)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(all + 1)
+    countAverage(good, neutral, bad+1)
+    countPositive(good, neutral, bad+1)
+  }
+
+  const countAverage = (good, neutral, bad) => {
+    const sum = good + bad + neutral
+    const avg = (good - bad) / sum
+    setAverage(avg)
+  }
+
+  const countPositive = (good, neutral, bad) => {
+    const sum = good + bad + neutral
+    const pos = good / sum
+    setPositive(pos*100 + '%')
   }
 
   return (
@@ -42,6 +68,9 @@ const App = () => {
        <Statistics stat={good} texti='good' />
        <Statistics stat={neutral} texti='neutral' />
        <Statistics stat={bad} texti='bad' />
+       <Statistics stat={all} texti='all' />
+       <Statistics stat={average} texti='average' />
+       <Statistics stat={positive} texti='positive' />
     </div>
   )
 }
