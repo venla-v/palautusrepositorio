975bd940261f9ac4d18a5eba01fdd046d0efe341
vemaveij <venla.m.veijalainen@jyu.fi>
2025-03-05 15:49:58 +0200

    1.8

--- osa1/Unicafe/src/App.jsx
+++ osa1/Unicafe/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Statistics = ({ stat, texti }) => {
+const StatisticsText = ({ stat, texti }) => {
   return (
     <div>
       <p>{texti} {stat}</p>
@@ -14,48 +14,41 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
+const Statistics = (props) => {
+  const avg = ((props.good-props.bad)/props.all)
+  const pos = (props.good/props.all)*100
+
+  return (
+    <>
+    <StatisticsText stat={props.good} texti='good' />
+    <StatisticsText stat={props.neutral} texti='neutral' />
+    <StatisticsText stat={props.bad} texti='bad' />
+    <StatisticsText stat={props.all} texti='all' />
+    <StatisticsText stat={avg} texti='average' />
+    <StatisticsText stat={pos+'%'} texti='positive' />
+    </>
+  )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
   const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
-    
     setGood(good + 1)
     setAll(all + 1)
-    countAverage(good+1, neutral, bad)
-    countPositive(good+1, neutral, bad)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
     setAll(all + 1)
-    countAverage(good, neutral +1, bad)
-    countPositive(good, neutral+1, bad)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
     setAll(all + 1)
-    countAverage(good, neutral, bad+1)
-    countPositive(good, neutral, bad+1)
-  }
-
-  const countAverage = (good, neutral, bad) => {
-    const sum = good + bad + neutral
-    const avg = (good - bad) / sum
-    setAverage(avg)
-  }
-
-  const countPositive = (good, neutral, bad) => {
-    const sum = good + bad + neutral
-    const pos = good / sum
-    setPositive(pos*100 + '%')
   }
 
   return (
@@ -65,12 +58,7 @@ const App = () => {
        <Button onClick={handleNeutralClick} text='neutral' />
        <Button onClick={handleBadClick} text='bad' />
        <h1>statistics</h1>
-       <Statistics stat={good} texti='good' />
-       <Statistics stat={neutral} texti='neutral' />
-       <Statistics stat={bad} texti='bad' />
-       <Statistics stat={all} texti='all' />
-       <Statistics stat={average} texti='average' />
-       <Statistics stat={positive} texti='positive' />
+       <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   )
 }
