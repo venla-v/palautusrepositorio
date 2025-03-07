b0c63b3a11b4a04728066296bcd40639e6ab39be
vemaveij <venla.m.veijalainen@jyu.fi>
2025-03-07 10:55:41 +0200

    2.9

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -4,15 +4,28 @@ const Persons = ({ person }) => {
   return <li>{person.name} {person.number}</li>
 }
 
+const Filter = ({ handleSearch }) => {
+  return <input onChange={handleSearch}/>
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', 
       number: '123 456 7890'
+    },
+    { name: 'Mato Matala', 
+      number: '098 765 4321'
+    },
+    { name: 'Lari Lehmä', 
+      number: '040 040 0400'
+    },
+    { name: 'Kevin Minion', 
+      number: '999 999 9999'
     }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
 
   const addNameNumber = (event) => {
@@ -36,8 +49,14 @@ const App = () => {
       setNewName('')
       setNewNumber('')
     }
+  }
 
-   
+  const personsToShow = search
+    ? persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+    : persons
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)  
   }
 
   const handleNameChange = (event) => {
@@ -51,22 +70,26 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        search: <Filter handleSearch={handleSearch}/>
+      </div>   
       <form onSubmit={addNameNumber}>
         <div>
-        <div>name: <input alue={newName} onChange={handleNameChange}/></div>
-        <div>number: <input alue={newNumber} onChange={handleNumberChange}/></div>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <Persons key={person.name} person={person} />
         ))}
       </ul>
     </div>
   )
 
-}
+  }
+
 
 export default App
