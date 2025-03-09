c1cf2f0c1420fa4bd3d2cb1cb5539843f20d88ef
vemaveij <venla.m.veijalainen@jyu.fi>
2025-03-09 16:20:52 +0200

    2.19

--- osa2/maidentiedot/src/App.jsx
+++ osa2/maidentiedot/src/App.jsx
@@ -8,7 +8,7 @@ const Image = ( {url} ) => {
   )
 }
 
-const Countries = ({ country }) => {
+const Countries = ({ country, showInfo }) => {
   if (!country) {
     return null
   }
@@ -23,50 +23,46 @@ const Countries = ({ country }) => {
     console.log(country)
     console.log(country[0].languages)
 
-    const countryLanguages = Object.values(country[0].languages)
-    
-    console.log(countryLanguages)
-
     return (
-      <div>
-
-      <h2>
-        {country[0].name.common}
-      </h2>
-      <div>
-        Capital {country[0].capital}
-      </div>
-      <div>
-        Area {country[0].area}
-      </div>
-      <h2>
-        Languages
-      </h2>
-      <div>
-      {countryLanguages.map((language) => (
-           <div key={language}>{language}</div>
-        ))}
-      </div>
-      <div>
-        <Image url={country[0].flags.png}/>
-      </div>
-
-      </div>
-      
+      <CountryInfo country={country[0]}/>
     )
   }
 
   return ( 
       <div> {country.map(c => (
-        <div key={c.name.common}>{c.name.common}</div>
+        <div key={c.name.common}>{c.name.common}
+        <button onClick={() => showInfo(c)}>show</button>
+        </div>
       ))}</div>
   )
 }
+
+const CountryInfo = ({ country }) => {
+  const countryLanguages = Object.values(country.languages);
+
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <div>Capital: {country.capital}</div>
+      <div>Area: {country.area}</div>
+      <h2>Languages</h2>
+      <div>
+        {countryLanguages.map((language) => (
+          <div key={language}>{language}</div>
+        ))}
+      </div>
+      <div>
+        <Image url={country.flags.png} />
+      </div>
+    </div>
+  );
+};
 
 
 const App = () => {
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
+  const [shown, setShown] = useState(null);
 
 
   useEffect(() => {
@@ -77,22 +73,43 @@ const App = () => {
 
   const handleChange = (event) => {
     setSearch(event.target.value)
+    setShown(null)
+  }
+
+  const showInfo = (country) => {
+    console.log(country)
+    setShown(country)
   }
 
   const countriesShown = search
    ? countries.filter(c => c.name.common.toLowerCase().includes(search.toLowerCase()))
    : countries
 
+if (!shown) {
   return (
     <div>
       <div>
         find countries: <input value={search} onChange={handleChange} />
       </div>
       <div>
-      <Countries country={countriesShown}/>
+      <Countries country={countriesShown} showInfo={showInfo}/>
       </div>
     </div>
   )
+}
+
+return (
+  <div>
+    <div>
+      find countries: <input value={search} onChange={handleChange} />
+    </div>
+    <div>
+    <Countries country={countriesShown} showInfo={showInfo}/>
+    <CountryInfo country={shown}/>
+    </div>
+  </div>
+)
+
 }
 
 export default App
