122aff9360034ce2916c696133644ac19a1146ec
vemaveij <venla.m.veijalainen@jyu.fi>
2025-04-14 16:13:16 +0300

    4.5

--- osa4/blogilista/backend/utils/list_helper.js
+++ osa4/blogilista/backend/utils/list_helper.js
@@ -7,7 +7,23 @@ const totalLikes = (blogs) => {
     const likes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
     return likes
   }
+
+const favoriteBlog = (blogs) => {
+
+  if (blogs.length === 1){
+    return blogs[0]
+  }
+  else {
+  let favoriteBlog = blogs[0]
+
+  for (let i = 1; i < blogs.length; i++){
+    if (favoriteBlog.likes < blogs[i].likes)
+      favoriteBlog = blogs[i]
+  }
+  return favoriteBlog
+}
+}
   
   module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
