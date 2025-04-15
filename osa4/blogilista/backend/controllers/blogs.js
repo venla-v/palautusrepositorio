fb3d7236a3971a90ce9a76005a6937e2d78d8707
vemaveij <venla.m.veijalainen@jyu.fi>
2025-04-15 15:27:51 +0300

    4.15

--- osa4/blogilista/backend/controllers/blogs.js
+++ osa4/blogilista/backend/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
+
 blogsRouter.get('/', (request, response) => {
     Blog.find({}).then(blogs => {
       response.json(blogs)
--- osa4/blogilista/backend/tests/blog_api.test.js
+++ osa4/blogilista/backend/tests/blog_api.test.js
@@ -6,6 +6,7 @@ const app = require('../app')
 
 const api = supertest(app)
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
     {
